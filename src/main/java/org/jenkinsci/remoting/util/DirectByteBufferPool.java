03eb2ad4a090094ee2afe51fdacf43172c6b3265
Olivier Lamy <olamy@apache.org>
2022-11-14 18:53:03 +1000

    test with Jetty byte buffer pool
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/main/java/org/jenkinsci/remoting/util/DirectByteBufferPool.java
+++ src/main/java/org/jenkinsci/remoting/util/DirectByteBufferPool.java
@@ -23,35 +23,16 @@
  */
 package org.jenkinsci.remoting.util;
 
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
-import net.jcip.annotations.GuardedBy;
+import org.eclipse.jetty.io.ArrayByteBufferPool;
 
 /**
- * A buffer pool that keeps a free list of {@link ByteBuffer}s of a specified default size in a simple fixed
- * size stack.
- *
- * If the stack is full, the buffer is de-referenced and available to be freed by normal garbage collection (whenever
- * that may actually take place)
- *
- * @see <a href="http://bugs.java.com/view_bug.do?bug_id=4469299">JDK-4469299</a>
- * @see <a href="http://www.evanjones.ca/java-bytebuffer-leak.html">More detail</a>
+ * Wrapper of the top of {@link ArrayByteBufferPool}
  */
 public class DirectByteBufferPool implements ByteBufferPool {
-    /**
-     * The pool of {@link ByteBuffer}. Only the first {@link #poolCount} elements will contain buffers.
-     */
-    @GuardedBy("this")
-    private final ByteBuffer[] pool;
-    /**
-     * The minimum size to allocate buffers.
-     */
-    private final int bufferSize;
-    /**
-     * The number of buffers currently held in the pool.
-     */
-    @GuardedBy("this")
-    private int poolCount;
+
+
+    private org.eclipse.jetty.io.ByteBufferPool byteBufferPool;
 
     /**
      * Constructor.
@@ -59,9 +40,10 @@ public class DirectByteBufferPool implements ByteBufferPool {
      * @param maxPoolSize the maximum buffers to keep in the pool.
      */
     public DirectByteBufferPool(int minBufferSize, int maxPoolSize) {
-        this.pool = new ByteBuffer[maxPoolSize];
-        this.bufferSize = minBufferSize;
-        this.poolCount = 0;
+        // int minCapacity, int factor, int maxCapacity, int maxBucketSize, long maxHeapMemory,
+        // long maxDirectMemory, long retainedHeapMemory, long retainedDirectMemory
+        this.byteBufferPool =
+                new ArrayByteBufferPool(minBufferSize, 16, -1, -1, 0, 0, 0, 0);
     }
 
     /**
@@ -69,31 +51,7 @@ public class DirectByteBufferPool implements ByteBufferPool {
      */
     @Override
     public ByteBuffer acquire(int size) {
-        synchronized (this) {
-            if (poolCount > 0) {
-                if (size <= bufferSize) {
-                    // anyone will do
-                    poolCount--;
-                    ByteBuffer result = pool[poolCount];
-                    pool[poolCount] = null; // drop reference
-                    ((Buffer) result).limit(size);
-                    return result;
-                }
-                for (int i = 0; i < poolCount; i++) {
-                    if (pool[i].capacity() >= size) {
-                        ByteBuffer result = pool[i];
-                        poolCount--;
-                        pool[i] = pool[poolCount]; // swap the big for a small
-                        pool[poolCount] = null; // drop reference
-                        ((Buffer) result).limit(size);
-                        return result;
-                    }
-                }
-            }
-        }
-        ByteBuffer result = ByteBuffer.allocateDirect(Math.max(bufferSize, size));
-        ((Buffer) result).limit(size);
-        return result;
+        return byteBufferPool.acquire(size, true);
     }
 
     /**
@@ -101,14 +59,6 @@ public class DirectByteBufferPool implements ByteBufferPool {
      */
     @Override
     public void release(ByteBuffer buffer) {
-        if (buffer.isDirect() && !buffer.isReadOnly() && buffer.capacity() >= bufferSize) {
-            synchronized (this) {
-                // we will let GC tidy any that are smaller than our size
-                if (poolCount < pool.length) {
-                    ((Buffer) buffer).clear();
-                    pool[poolCount++] = buffer;
-                }
-            }
-        }
+        byteBufferPool.release(buffer);
     }
 }
---
+++ src/test/java/org/jenkinsci/remoting/util/ByteBufferPoolBenchmark.java
@@ -0,0 +1,69 @@
+package org.jenkinsci.remoting.util;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.nio.ByteBuffer;
+
+@State(Scope.Benchmark)
+public class ByteBufferPoolBenchmark
+{
+
+    @Param({
+            "simple",
+            "jetty"
+    })
+    public static String POOL_TYPE;
+
+    private ByteBufferPool pool;
+
+    @Setup
+    public void setUp() throws Exception
+    {
+        switch (POOL_TYPE) {
+            case "simple" :
+                pool = new SimpleDirectByteBufferPool(16916, Runtime.getRuntime().availableProcessors() * 4);
+                break;
+            case "jetty" :
+                pool = new DirectByteBufferPool(16916, Runtime.getRuntime().availableProcessors() * 4);
+                break;
+            default:
+                throw new IllegalStateException();
+        }
+    }
+
+    @TearDown
+    public void tearDown()
+    {
+        pool = null;
+    }
+
+    @Benchmark
+    public void testAcquireRelease()
+    {
+        ByteBuffer buffer = pool.acquire(8192);
+        pool.release(buffer);
+    }
+
+    public static void main(String[] args) throws RunnerException
+    {
+        Options opt = new OptionsBuilder()
+            .include(ByteBufferPoolBenchmark.class.getSimpleName())
+            .warmupIterations(3)
+            .measurementIterations(3)
+            .forks(1)
+            .threads(8)
+            // .addProfiler(GCProfiler.class)
+            .build();
+
+        new Runner(opt).run();
+    }
+}
