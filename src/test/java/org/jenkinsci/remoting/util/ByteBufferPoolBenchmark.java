8a7a2181c01fdb36e177d97e1224477dabbe1ca0
Olivier Lamy <olamy@apache.org>
2022-11-15 13:56:49 +1000

    add more measurement
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/jenkinsci/remoting/util/ByteBufferPoolBenchmark.java
+++ src/test/java/org/jenkinsci/remoting/util/ByteBufferPoolBenchmark.java
@@ -1,6 +1,9 @@
 package org.jenkinsci.remoting.util;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
@@ -14,6 +17,7 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 import java.nio.ByteBuffer;
 
 @State(Scope.Benchmark)
+@BenchmarkMode({Mode.Throughput,Mode.AverageTime})
 public class ByteBufferPoolBenchmark
 {
 
